16d9f7fbd60fe095168d0f672995d73c13578e81
Ivan Backovic <ng1v4n85@gmail.com>
2023-06-16 15:41:28 +0200

    Reactive_ Getting Access to Controls

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     this.signupForm = new FormGroup({
       'username': new FormControl(null, Validators.required),
-      'emial': new FormControl(null, [Validators.required, Validators.email]),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
       'gender': new FormControl('male')
     });
   }
